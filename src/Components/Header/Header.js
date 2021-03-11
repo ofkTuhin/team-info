f2240a909ac9e014bc8ed76608349809a4d96821
omar-faruk02 <tuhin.omar99@gmail.com>
2021-03-11 06:38:14 -0800

    commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,24 +8,24 @@ const Header = () => {
         <div>
             <nav class="navbar navbar-expand-lg  main-menu">
   <div class="container-fluid">
-      <Link to="/"><a class="navbar-brand logo"><img src={logo} alt="logo"></img></a></Link>
+      <Link to="/"><a class="navbar-brand logo" href="index.html"><img src={logo} alt="logo"></img></a></Link>
     
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon button"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ml-auto">
+      <Link to="/"><li class="nav-item navLi">
+        Home
+        </li></Link>
         <li class="nav-item navLi">
-        <Link to="/"> <a class="nav-link active" aria-current="page">Home</a></Link>
+        About
         </li>
         <li class="nav-item navLi">
-          <a class="nav-link" >About</a>
+       History
         </li>
         <li class="nav-item navLi">
-          <a class="nav-link " >History</a>
-        </li>
-        <li class="nav-item navLi">
-          <a class="nav-link">Achievements</a>
+          Achievements
         </li>
       </ul>
     </div>
